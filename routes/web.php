31e96e6ea2cc38f10893bae00351f8992879f710
Alessandro Calu <alessandrocalu@gmail.com>
2021-12-13 00:23:36 -0300

    create routes

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/articles/', function () {
+    return view('welcome');
+});
+
+Route::get('/articles/{id}', function () {
+    return view('welcome');
+});
